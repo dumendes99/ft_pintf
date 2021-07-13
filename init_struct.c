305a368c732a5e30ce02ab36d794abd57b47aeec
dumendes99 <dum3ndes@gmail.com>
2021-07-13 17:10:43 -0300

    will be test in guaca

--- init_struct.c
+++ init_struct.c
@@ -2,10 +2,11 @@
 
 void	init_struct(t_flags *s_flags)
 {
-	s_flags->sign = 0;
+	s_flags->minus = 0;
 	s_flags->zero = 0;
 	s_flags->width = 0;
-	s_flags->prec = 0;
+	s_flags->dot = 0;
 	s_flags->index = 0;
 	s_flags->size = 0;
+	s_flags->plus = 0;
 }
