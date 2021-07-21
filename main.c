2e15ecc00b5b2d9cc0b70b42c9ce36939565d392
dumendes99 <dum3ndes@gmail.com>
2021-07-21 14:07:56 -0300

    fix return printf

--- main.c
+++ main.c
@@ -6,52 +6,52 @@ int main(void)
 	int hex = 15643;
 	char c = 'e';
 	char *str = "eduardo";
-
+ 
 	printf(" %d\n", ft_printf("qualquer coisa| meu "));
 	printf(" %d\n\n", printf("qualquer coisa| std "));
-
+ 
 	printf(" %d\n", ft_printf("%-010d| meu ", n));
 	printf(" %d\n\n", printf("%-010d| std " , n));
-
+ 
 	printf(" %d\n", ft_printf("%010d| meu ", n));
 	printf(" %d\n\n", printf("%010d| std " , n));
-
+ 
 	printf(" %d\n", ft_printf("%10d| meu ", n));
 	printf(" %d\n\n", printf("%10d| std " , n));
-
+ 
 	printf(" %d\n", ft_printf("%d| meu ", n));
 	printf(" %d\n\n", printf("%d| std " , n));
-
+ 
 	printf(" %d\n", ft_printf("%c| meu ", c));
 	printf(" %d\n\n", printf("%c| std ", c));
-
+ 
 	printf(" %d\n", ft_printf("%s| meu ", str));
 	printf(" %d\n\n", printf("%s| std ", str));
-
+ 
 	printf(" %d\n", ft_printf("%015i| meu ", n));
 	printf(" %d\n\n", printf("%015i| std ", n));
-
+ 
 	printf(" %d\n", ft_printf("%015u| meu ", n));
 	printf(" %d\n\n", printf("%015u| std ", n));
-
+ 
 	printf(" %d\n", ft_printf("%020p| meu ", &str));
 	printf(" %d\n\n", printf("%020p| std ", &str));
-
+ 
 	printf(" %d\n", ft_printf("%-20p| meu ", &str));
 	printf(" %d\n\n", printf("%-20p| std ", &str));
-
+ 
 	printf(" %d\n", ft_printf("%20p| meu ", &str));
 	printf(" %d\n\n", printf("%20p| std ", &str));
-
+ 
 	printf(" %d\n", ft_printf("%p| meu ", &str));
 	printf(" %d\n\n", printf("%p| std ", &str));
-
+ 
 	printf(" %d\n", ft_printf("%015x| meu ", hex));
 	printf(" %d\n\n", printf("%015x| std ", hex));
-
+ 
 	printf(" %d\n", ft_printf("%015X| meu ", hex));
 	printf(" %d\n\n", printf("%015X| std ", hex));
-
+ 
 	printf(" %d\n", ft_printf("%%| meu "));
 	printf(" %d\n", printf("%%| std "));
 }
--- print_width.c
+++ print_width.c
@@ -5,7 +5,7 @@ void	print_width(int *rest_size, t_flags *s_flags)
 	if ((s_flags->minus == 1) && (s_flags->zero == 1))
 		s_flags->zero = 0;
 	if ((s_flags->zero) && (*rest_size > 0))
-		while (*rest_size)
+		while (*rest_size > 0)
 		{
 			ft_putchar_fd('0', 1, s_flags);
 			*rest_size = *rest_size - 1;
